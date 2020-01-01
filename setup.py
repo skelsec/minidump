3677e1bdb9f7d557cd3d07834275fa0e669f325a
SkelSec <info@skelsec.com>
2020-01-01 16:49:57 +0100

    minidump shell, new versoning

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from setuptools import setup, find_packages
+import re
+
+VERSIONFILE="minidump/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 setup(
 	# Application name:
 	name="minidump",
 
 	# Version number (initial):
-	version="0.0.11",
+	version=verstr,
 
 	# Application author details:
 	author="Tamas Jos",
-	author_email="info@skelsec.com",
+	author_email="skelsecprojects@gmail.com",
 
 	# Packages
 	packages=find_packages(),
