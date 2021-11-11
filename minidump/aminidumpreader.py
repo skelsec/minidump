e587355a9bf6c14283d1d659e52b3289de92a9f7
physics-sec <physics-sec@protonmail.com>
2021-11-11 17:54:00 -0300

    use '==' instead of 'find' in get_module_by_name

--- minidump/aminidumpreader.py
+++ minidump/aminidumpreader.py
@@ -308,7 +308,7 @@ class AMinidumpFileReader:
 
 	def get_module_by_name(self, module_name):
 		for mod in self.modules:
-			if ntpath.basename(mod.name.lower()).find(module_name.lower()) != -1:
+			if ntpath.basename(mod.name).lower() == module_name.lower():
 				return mod
 		return None
 
--- minidump/minidumpreader.py
+++ minidump/minidumpreader.py
@@ -319,7 +319,7 @@ class MinidumpFileReader:
 
 	def get_module_by_name(self, module_name):
 		for mod in self.modules:
-			if ntpath.basename(mod.name.lower()).find(module_name.lower()) != -1:
+			if ntpath.basename(mod.name).lower() == module_name.lower():
 				return mod
 		return None
 
